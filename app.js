13a010ce144aeab76487d9ce09058875287ef55b
reviewtechsmasher <85062559+reviewtechsmasher@users.noreply.github.com>
2023-12-14 20:06:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-// morden bottom menu 
 
-// const list = document.querySelectorAll('.list');
-// function activelink(){
-//     list.forEach((item)=>
-//     item.classList.remove('active'));
-//     this.classList.add('active');
-// }
-// list.forEach((item)=>
-// item.addEventListener('click', activelink));
-
-
-// const toggleButton = document.querySelector('.toggle-button');
-// const navbar = document.querySelector('.navbar');
-
-// toggleButton.addEventListener('click', () => {
-//     navbar.classList.toggle('act');
-// });
-
+//toggle control
 const navbar = document.querySelector('.navbar');
 document.querySelector('.toggle-button').addEventListener('click', function() {
     this.classList.toggle('change');
@@ -30,13 +13,28 @@ document.getElementById('m').addEventListener('click', function() {
     document.querySelector('.toggle-button').classList.toggle('change');
     navbar.classList.toggle('act');
    });
+   
+
+const contact = document.querySelector('.pop');
+document.querySelector('.cross').addEventListener('click', function() {
+    contact.classList.toggle('close');
+})
+document.querySelector('.submit-btn').addEventListener('click', function() {
+    contact.classList.toggle('close');
+})
+document.querySelector('.contact').addEventListener('click', function() {
+    contact.classList.toggle('close');
+})
 
 
-
+//animate on scroll
    window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
 
+
+
+  //swiper js
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 'auto',
     spaceBetween: 30,
